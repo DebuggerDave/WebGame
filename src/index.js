d6d824aa5986129a027f950bff65e5dbb113d4d1
David Schwartz <dmschwartz01@gmail.com>
2021-02-02 20:43:33 -0600

    rename app.js to index.js, change where files are served in up.sh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+
+class App extends React.Component{
+    render(){
+        return(
+            <img src="arms_front.gif" alt="Big Boi"></img>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
