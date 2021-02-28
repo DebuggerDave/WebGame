435c5861da7ef3b7b2e9308e419250ca47f552fc
DavSchwartz <dmschwartz01@gmail.com>
2021-02-27 21:12:00 -0600

    remove database stuff

--- createTables.sql
+++
@@ -1,8 +0,0 @@
-DROP SCHEMA IF EXISTS smugbug CASCADE;
-CREATE SCHEMA smugbug
-CREATE TABLE smugbug.user (
-	id integer PRIMARY KEY,
-	lastname varchar(255) NOT NULL,
-	firstname varchar(255) NOT NULL,
-	email varchar(255) NOT NULL
-);
--- initDB.sh
+++
@@ -1,1 +0,0 @@
-psql --dbname=rubeus --file=createTables.sql
